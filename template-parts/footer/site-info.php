9ad3d27ca5572a4c3376ac8d9125c2ba13cc4efe
Kimberly Pennington <kimberlythegeek@gmail.com>
2016-12-23 20:57:41 -0700

    added design credit

---
+++ template-parts/footer/site-info.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Displays footer site info
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+<div class="site-info">
+	<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyseventeen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentyseventeen' ), 'WordPress' ); ?></a>
+  <div class="design-credit">
+    Website by <a href="http://kimberly.codes">Kimberly Pennington</a> of <a href="http://acodeabove.com">A Code Above</a>
+  </div>
+</div><!-- .site-info -->
