08da757f1bc2344d3d33dd37512c1afe51ba52a3
Kimberly Pennington <kimberlythegeek@gmail.com>
2016-12-29 10:03:07 -0700

    style changes

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+  function twentyseventeen_child_widgets_init() {
+  	register_sidebar( array(
+  		'name'          => __( 'Left Sidebar', 'twentyseventeen-child' ),
+  		'id'            => 'sidebar-left',
+  		'description'   => __( 'Add widgets here to appear in your left sidebar.', 'twentyseventeen' ),
+  		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+  		'after_widget'  => '</section>',
+  		'before_title'  => '<h2 class="widget-title">',
+  		'after_title'   => '</h2>',
+  	) );
+  }
+?>
---
+++ sidebar-left.php
@@ -0,0 +1,10 @@
+<?php
+
+if ( ! is_active_sidebar( 'sidebar-left' ) ) {
+	return;
+}
+?>
+
+<aside id="secondary" class="widget-area" role="complementary">
+	<?php dynamic_sidebar( 'sidebar-left' ); ?>
+</aside><!-- #secondary -->
