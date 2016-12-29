cbe257b9b25b9813082fa72a3124265949f550ac
Kimberly Pennington <kimberlythegeek@gmail.com>
2016-12-29 15:39:52 -0700

    added left-sidebar to index.php

--- sidebar-left.php
+++ sidebar-left.php
@@ -5,6 +5,6 @@ if ( ! is_active_sidebar( 'sidebar-left' ) ) {
 }
 ?>
 
-<aside id="secondary" class="widget-area" role="complementary">
+<aside id="secondary" class="widget-area left-sidebar" role="complementary">
 	<?php dynamic_sidebar( "sidebar-left" ); ?>
 </aside><!-- #secondary -->
