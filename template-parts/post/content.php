dd1e20d1f6378ab8ae3e1eb11b0c1ffb5d77be8e
Kimberly Pennington <kimberlythegeek@gmail.com>
2017-01-17 09:09:56 -0700

    oops

--- template-parts/post/content.php
+++ template-parts/post/content.php
@@ -83,7 +83,7 @@
         </div><!-- .about-text -->
       <br />
       <div class="about-photo col-xs-2">
-        <img src="http://localhost/wordpress/wp-content/uploads/2016/12/profile.jpg" alt="lamp stack kimberly pennington kimberlythegeek"/>
+        <img src="/wp-content/uploads/2016/12/profile.jpg" alt="lamp stack kimberly pennington kimberlythegeek"/>
       </div><!-- .about-photo -->
       </p>
 
